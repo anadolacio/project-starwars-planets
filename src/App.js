1bd276241b844ec4d3bccaf795769d9cdc1de508
anadolacio <anadolacio@hotmail.com>
2023-02-28 09:56:12 -0300

    resolução requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import SearchPlanetsContext from './context/SearchPlanetsContext';
+import Table from './components/Table';
 
 function App() {
+  const url = 'https://swapi.dev/api/planets';
+  const [planets, setPlanets] = useState([]);
+  const [selectedPlanets, setSelectPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const planetsSearched = data.results.map((planet) => {
+          const { residents, ...planetSearched } = planet;
+          // console.log(planetSearched);
+          return planetSearched;
+        });
+        setPlanets(planetsSearched);
+      });
+  }, []);
+
+  useEffect(() => {
+    const filteredPlanets = planets.filter((planet) => planet.name);
+    setSelectPlanets(filteredPlanets);
+    // console.log(filteredPlanets);
+  }, [planets]);
+
+  const contexValue = {
+    planets,
+    selectedPlanets,
+  };
+
   return (
-    <span>Hello, App!</span>
+    <SearchPlanetsContext.Provider value={ contexValue }>
+      <Table />
+    </SearchPlanetsContext.Provider>
   );
 }
 
